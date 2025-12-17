3053bd052d865defdd8dc483e847b8f8ae178533
DikiYLexa <dubovan36@gmail.com>
2025-12-17 13:34:32 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIGigaChat
+{
+    public class Program
+    {
+        public static string ClientId = "";
+        public static string AutorizationKey = "";
+        static void Main(string[] args)
+        {
+        }
+    }
+}
