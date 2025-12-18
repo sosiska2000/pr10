5dac02ebf4880835134ae6aeb6d89801ceac6ffa
qwelord <secondofall22@rambler.ru>
2025-12-18 15:22:27 +0500

    Разработанный метод для получения токена пользователя.Komornyi

--- Program.cs
+++ Program.cs
@@ -1,23 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
+using APIGigaChat.Models.Response;
+using Newtonsoft.Json;
 
 namespace APIGigaChat
 {
     public class Program
     {
-        public static string ClientId = "019b2b50-ff41-77e2-b0f5-23e0ba29e4ef";
-        public static string AutorizationKey = "MDE5YjJiNTAtZmY0MS03N2UyLWIwZjUtMjNlMGJhMjllNGVmOmVhZWM1MDcxLWU2ZWYtNDRmMi1iZjZhLTE0NGY1YzRjMjQxMA==";
+        public static string ClientId = "";
+        public static string AutorizationKey = "";
         static void Main(string[] args)
         {
         }
         public static async Task<string> GetToken(string rqUID, string bearer)
         {
             string ReturnToken = null;
-            string Url = "";
+            string Url = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth";
 
+            using (HttpClientHandler Handler = new HttpClientHandler())
+            {
+                Handler.ServerCertificateCustomValidationCallback = (message, sert, chain, SslPolicyErrors) => true;
+
+                using (HttpClient Client = new HttpClient(Handler))
+                {
+                    HttpRequestMessage Request = new HttpRequestMessage(HttpMethod.Post, Url);
+
+                    Request.Headers.Add("Accept", "application/json");
+                    Request.Headers.Add("RqUID", rqUID);
+                    Request.Headers.Add("Authorization", $"Bearer{bearer}");
+
+                    var Data = new List<KeyValuePair<string, string>>
+                    {
+                        new KeyValuePair<string, string>("scope", "GIGACHAT_API_PERS")
+                    };
+
+                    Request.Content = new FormUrlEncodedContent(Data);
+
+                    HttpResponseMessage Response = await Client.SendAsync(Request);
+
+                    if (Response.IsSuccessStatusCode)
+                    {
+                        string ResponseContent = await Response.Content.ReadAsStringAsync();
+                        ResponseToken Token = JsonConvert.DeserializeObject<ResponseToken>(ResponseContent);
+
+                        ReturnToken = Token.access_token;
+                    }
+                }
+            }
             return ReturnToken;
         }
     }
